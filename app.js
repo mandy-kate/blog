9622b02f0f06292404501db6b428bf9f63fe39fb
mandy-kate <mandykatebaker@gmail.com>
2017-04-29 15:20:49 +1200

    update app.js to listen

--- app.js
+++ app.js
@@ -7,12 +7,6 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Listen for requests
-// var server = app.listen(app.get('port'), function() {
-//   var port = server.address().port;
-//   console.log('Magic happens on port ' + port);
-// });
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
